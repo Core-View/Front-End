80cef3465e1dde1f18c8f80305152e9e39692c79
hnk1203 <hnk1203@naver.com>
2024-06-05 04:46:02 +0900

    관리자페이지 틀 제작

---
+++ front/src/Admin/Admin_notice.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./admin_notice.css";
+
+const AdminNotice = () => {
+  return (
+    <div className="admin_notice_container">
+      <ul className="ad_notice_list">
+        <li>1.asdfasdfasdf</li>
+        <li>1.asdfasdfasdf</li>
+        <li>1.asdfasdfasdf</li>
+        <li>1.asdfasdfasdf</li>
+      </ul>
+    </div>
+  );
+};
+
+export default AdminNotice;
