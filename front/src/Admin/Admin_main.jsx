9c2af37763f8a6d39836da5b177999ff574992b5
hnk1203 <hnk1203@naver.com>
2024-06-12 06:00:15 +0900

    멤버기여도 한눈보기 완성, 멤버관리 진행중

--- front/src/Admin/Admin_main.jsx
+++ front/src/Admin/Admin_main.jsx
@@ -1,49 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./admin_main.css";
 import { GrScorecard } from "react-icons/gr";
 import { AiFillCaretRight } from "react-icons/ai";
 import AdNotice from "./Admin_notice";
 import AdPost from "./Admin_post";
+import axios from "axios";
 
 const Admin = () => {
   const [selectedMenu, setSelectedMenu] = useState("어떤관리");
+  const [member, setMember] = useState([]);
 
-  const member = [
-    { name: "김민수", point: 10000 },
-    { name: "이유진", point: 20000 },
-    { name: "박지훈", point: 30000 },
-    { name: "정수연", point: 40000 },
-    { name: "최영준", point: 20230 },
-    { name: "홍길동", point: 20044 },
-    { name: "이서연", point: 20002 },
-    { name: "강민준", point: 2000 },
-    { name: "윤지호", point: 2000 },
-    { name: "신예은", point: 20002 },
-    { name: "오준서", point: 21000 },
-    { name: "배수지", point: 2000 },
-    { name: "장우영", point: 2000 },
-    { name: "임나영", point: 20300 },
-    { name: "서준혁", point: 2000 },
-    { name: "권지민", point: 20050 },
-    { name: "한예슬", point: 2000 },
-    { name: "조승우", point: 2000 },
-    { name: "문지후", point: 20200 },
-    { name: "유진아", point: 20055 },
-    { name: "유진아", point: 20055 },
-    { name: "유진아", point: 20055 },
-    { name: "유진아", point: 20055 },
-    { name: "유진아", point: 20055 },
-  ];
+  const getMember = () => {
+    axios.get("http://localhost:3000/notice/viewuser").then((response) => {
+      if (response.data.success === true) {
+        console.log(response.data.user);
+        setMember(response.data.user);
+      }
+    });
+  };
 
-  const maxPoint = member.reduce(
-    (max, { point }) => (point > max ? point : max),
-    0
-  );
+  useEffect(() => {
+    getMember();
+  }, []);
 
   const renderMenuContent = () => {
     switch (selectedMenu) {
-      case "어떤관리":
-        return <div>어떤관리 내용</div>;
+      case "회원관리":
+        return <div>회원관리 내용</div>;
       case "공지사항":
         return <AdNotice />;
       case "게시판관리":
@@ -53,23 +36,24 @@ const Admin = () => {
     }
   };
 
+  const maxContribute = Math.max(...member.map((m) => m.USER_CONTRIBUTE));
+
   return (
     <div className="admin-container">
       <div className="member">
-        <h3 className="admin_title">회원&기여도관리</h3>
+        <h3 className="admin_title">멤버&기여도현황</h3>
         <ul className="memberList">
           {member.map((a, i) => (
             <li key={i}>
-              <span>
+              <span className="nickname">
                 <AiFillCaretRight />
-                {a.name}&nbsp;&nbsp;
-                <GrScorecard />
+                <div>{a.USER_NICKNAME}</div>
               </span>
-              <span style={{ width: "100px" }}>{a.point}</span>
+              <span style={{ width: "100px" }}>{a.USER_CONTRIBUTE}</span>
               <div
                 className="gauge"
                 style={{
-                  width: `${(a.point / maxPoint) * 50}%`,
+                  width: `${(a.USER_CONTRIBUTE / maxContribute) * 50}%`,
                 }}
               ></div>
             </li>
@@ -79,7 +63,7 @@ const Admin = () => {
       <div className="admin_menu">
         <h3 className="admin_title">CoReview 관리자메뉴</h3>
         <ul className="admin_menu_list">
-          <li onClick={() => setSelectedMenu("어떤관리")}>어떤관리</li>
+          <li onClick={() => setSelectedMenu("회원관리")}>회원관리</li>
           <li onClick={() => setSelectedMenu("공지사항")}>공지사항</li>
           <li onClick={() => setSelectedMenu("게시판관리")}>게시판관리</li>
         </ul>
