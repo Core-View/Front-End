d6bb2646fa0b5a30b35d625597ab1f6e8dd46d23
hnk1203 <hnk120325@gmail.com>
2024-06-14 16:51:01 +0900

    회원관리 완료

--- front/src/Admin/Admin_main.jsx
+++ front/src/Admin/Admin_main.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./admin_main.css";
-import { AiFillCaretRight } from "react-icons/ai";
+import { LuCrown } from "react-icons/lu";
 import AdNotice from "./Admin_notice";
 import AdPost from "./Admin_post";
 import AdUsers from "./Admin_user";
@@ -9,24 +9,30 @@ import axios from "axios";
 const Admin = () => {
   const [selectedMenu, setSelectedMenu] = useState("어떤관리");
   const [member, setMember] = useState([]);
+  const [clicked, setClicked] = useState([false, false, false]);
 
   const getMember = () => {
     axios.get("http://localhost:3000/notice/viewuser").then((response) => {
       if (response.data.success === true) {
-        console.log(response.data.user);
         setMember(response.data.user);
       }
     });
   };
 
-  useEffect(() => {
-    getMember();
-  }, [<AdUsers />]);
+  const handleDelete = (userId) => {
+    axios
+      .delete(`http://localhost:3000/mypage/${userId}/delete`)
+      .then((response) => {
+        if (response.data.success) {
+          getMember(); // 회원 삭제 후 목록을 새로 불러옴
+        }
+      });
+  };
 
   const renderMenuContent = () => {
     switch (selectedMenu) {
       case "회원관리":
-        return <AdUsers />;
+        return <AdUsers userdata={member} onDelete={handleDelete} />;
       case "공지사항":
         return <AdNotice />;
       case "게시판관리":
@@ -38,18 +44,31 @@ const Admin = () => {
 
   const maxContribute = Math.max(...member.map((m) => m.USER_CONTRIBUTE));
 
+  getMember();
+
   return (
     <div className="admin-container">
       <div className="member">
-        <h3 className="admin_title">멤버&기여도현황</h3>
+        <h3 className="admin_title">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;멤버&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&
+          &nbsp;&nbsp;기여도현황
+        </h3>
         <ul className="memberList">
           {member.map((a, i) => (
             <li key={i}>
               <span className="nickname">
-                <AiFillCaretRight />
+                <LuCrown
+                  style={{
+                    marginTop: "5px",
+                    marginRight: "5px",
+                    marginLeft: "5px",
+                  }}
+                />
                 <div>{a.USER_NICKNAME}</div>
               </span>
-              <span style={{ width: "100px" }}>{a.USER_CONTRIBUTE}</span>
+              <span style={{ width: "140px", lineHeight: "25px" }}>
+                {a.USER_CONTRIBUTE}
+              </span>
               <div
                 className="gauge"
                 style={{
@@ -63,9 +82,33 @@ const Admin = () => {
       <div className="admin_menu">
         <h3 className="admin_title">CoReview 관리자메뉴</h3>
         <ul className="admin_menu_list">
-          <li onClick={() => setSelectedMenu("회원관리")}>회원관리</li>
-          <li onClick={() => setSelectedMenu("공지사항")}>공지사항</li>
-          <li onClick={() => setSelectedMenu("게시판관리")}>게시판관리</li>
+          <li
+            className={`menu1 ${clicked[0] ? "nowshow" : ""}`}
+            onClick={() => {
+              setSelectedMenu("회원관리");
+              setClicked([true, false, false]);
+            }}
+          >
+            회원관리
+          </li>
+          <li
+            className={`menu2 ${clicked[1] ? "nowshow" : ""}`}
+            onClick={() => {
+              setSelectedMenu("공지사항");
+              setClicked([false, true, false]);
+            }}
+          >
+            공지사항
+          </li>
+          <li
+            className={`menu3 ${clicked[2] ? "nowshow" : ""}`}
+            onClick={() => {
+              setSelectedMenu("게시판관리");
+              setClicked([false, false, true]);
+            }}
+          >
+            게시판관리
+          </li>
         </ul>
         <div className="show_admin_menu">{renderMenuContent()}</div>
       </div>
--- front/src/Admin/Admin_user.jsx
+++ front/src/Admin/Admin_user.jsx
@@ -1,32 +1,7 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "./admin_user.css";
 
-const AdminUser = () => {
-  const [users, setUsers] = useState([]);
-
-  // 회원 데이터를 가져오는 함수 (예: API 호출)
-  const getUserData = () => {
-    axios.get("http://localhost:3000/notice/viewuser").then((response) => {
-      if (response.data.success === true) {
-        setUsers(response.data.user);
-      }
-    });
-  };
-
-  // 회원 삭제 함수
-  const handleDelete = (userId) => {
-    axios
-      .delete(`http://localhost:3000/mypage/${userId}/delete`)
-      .then((response) => {
-        getUserData(); // 삭제 후 데이터 다시 가져오기
-      });
-  };
-
-  useEffect(() => {
-    getUserData();
-  }, []);
-
+const AdminUser = ({ userdata, onDelete }) => {
   return (
     <div className="user_container">
       <table className="user_table">
@@ -41,19 +16,17 @@ const AdminUser = () => {
           </tr>
         </thead>
         <tbody>
-          {users.map((a, i) => (
-            <tr key={i}>
-              <td>{a.USER_ID}</td>
-              <td>{a.USER_NAME}</td>
-              <td>{a.USER_NICKNAME}</td>
-              <td>{a.USER_EMAIL}</td>
-              <td>{a.USER_CONTRIBUTE}</td>
+          {userdata.map((user, index) => (
+            <tr key={index}>
+              <td>{user.USER_ID}</td>
+              <td>{user.USER_NAME}</td>
+              <td>{user.USER_NICKNAME}</td>
+              <td>{user.USER_EMAIL}</td>
+              <td>{user.USER_CONTRIBUTE}</td>
               <td>
                 <button
                   className="delete-btn"
-                  onClick={(e) => {
-                    handleDelete(a.USER_ID);
-                  }}
+                  onClick={() => onDelete(user.USER_ID)}
                 >
                   삭제
                 </button>
