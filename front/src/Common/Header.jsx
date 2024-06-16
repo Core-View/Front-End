6db8798d996c4478b5d9aa424498a34e72b091e5
HyeongJun, Kim <khj029956@naver.com>
2024-06-16 14:49:16 +0900

    Feat: 헤더 로그아웃 시 글쓰기, 내정보 클릭 -> 로그인창

--- front/src/Common/Header.jsx
+++ front/src/Common/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-// npm install react-router-dom
-import { Link, redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './header.css';
 import Alarm from './Alarm';
 import { Cookies } from 'react-cookie';
@@ -8,13 +7,16 @@ import { Cookies } from 'react-cookie';
 function Header() {
   const cookies = new Cookies();
   const [userId, setUserId] = useState(cookies.get('user_id'));
+
   const deleteCookies = () => {
     cookies.remove('user_id');
     setUserId(undefined);
     window.location.reload();
   };
+
   return (
     <header className="header">
+      {/* <div className="header-padding"> */}
       <div className="header-logo-container">
         <Link to="/">
           <img
@@ -25,30 +27,45 @@ function Header() {
         </Link>
       </div>
       <nav className="header-nav">
-        <ul>
-          <li>
-            <Link to="/post_main">전체 게시글</Link>
-          </li>
-          <li>
-            <Link to="/post_write">글 쓰기</Link>
-          </li>
-          <li>
-            <Link to="/my_main">내 정보</Link>
-          </li>
-          <li>
-            {userId === undefined ? (
-              <Link to="/users/sign-in">로그인</Link>
-            ) : (
-              <Link to="/" onClick={deleteCookies}>
-                로그아웃
-              </Link>
-            )}
-          </li>
-          <li>
-            <Alarm />
-          </li>
-        </ul>
+        <div className="header-nav-left">
+          <ul>
+            <li>
+              <Link to="/post_main">전체 게시글</Link>
+            </li>
+            <li>
+              {userId === undefined ? (
+                <Link to="/users/sign-in">글 쓰기</Link>
+              ) : (
+                <Link to="/post_write">글 쓰기</Link>
+              )}
+            </li>
+            <li>
+              {userId === undefined ? (
+                <Link to="/users/sign-in">내 정보</Link>
+              ) : (
+                <Link to="/my_main">내 정보</Link>
+              )}
+            </li>
+          </ul>
+        </div>
+        <div className="header-nav-right">
+          <ul>
+            <li>
+              {userId === undefined ? (
+                <Link to="/users/sign-in">로그인</Link>
+              ) : (
+                <Link to="/" onClick={deleteCookies}>
+                  로그아웃
+                </Link>
+              )}
+            </li>
+            <li>
+              <Alarm />
+            </li>
+          </ul>
+        </div>
       </nav>
+      {/* </div> */}
     </header>
   );
 }
