cf1b3fd2fbd5659667a4b6470e0da86f3dfc8eaf
hnk1203 <hnk120325@gmail.com>
2024-06-20 18:31:42 +0900

    sorr

--- front/src/Common/Header.jsx
+++ front/src/Common/Header.jsx
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // npm install react-router-dom
-import { Link, redirect } from 'react-router-dom';
-import './header.css';
-import Alarm from './Alarm';
-import { Cookies } from 'react-cookie';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { Link, redirect } from "react-router-dom";
+import "./header.css";
+import Alarm from "./Alarm";
+import { Cookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Header() {
   const cookies = new Cookies();
-  const [userId, setUserId] = useState(cookies.get('user_id'));
-  const [role, setRole] = useState(cookies.get('role'));
+  const [userId, setUserId] = useState(cookies.get("user_id"));
+  const [role, setRole] = useState(cookies.get("role"));
   const deleteCookies = () => {
-    cookies.remove('user_id');
-    cookies.remove('role');
+    cookies.remove("user_id");
+    cookies.remove("role");
     setUserId(undefined);
     setRole(undefined);
     window.location.reload();
   };
 
   const navigate = useNavigate();
-
+  console.log("미안요");
   const clickedAdmin = () => {
-    let inputPassword = prompt('비밀번호를 입력하세요.', '');
+    let inputPassword = prompt("비밀번호를 입력하세요.", "");
     if (inputPassword) {
       axios
         .post(`http://localhost:3000/admin/login/${userId}`, {
@@ -32,14 +32,14 @@ function Header() {
         })
         .then((response) => {
           if (response.data.success === true) {
-            navigate('/admin');
+            navigate("/admin");
           } else {
-            alert('오류발생!');
-            navigate('/');
+            alert("오류발생!");
+            navigate("/");
           }
         });
     } else {
-      alert('오류발생!');
+      alert("오류발생!");
     }
   };
   return (
@@ -66,7 +66,7 @@ function Header() {
               관리자페이지
             </div>
           ) : (
-            ''
+            ""
           )}
           <li>
             <Link to="/post_main">전체 게시글</Link>
