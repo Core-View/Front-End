7b29520045b9f8feb46e4471acb0bf01ded00e3d
HyeongJun, Kim <khj029956@naver.com>
2024-06-23 04:53:58 +0900

    Doc: Header 이름 변경

--- front/src/Common/Header.jsx
+++ front/src/Common/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './header.css';
+import './Header.css';
 import Alarm from './Alarm';
 import { Cookies } from 'react-cookie';
 import axios from 'axios';
