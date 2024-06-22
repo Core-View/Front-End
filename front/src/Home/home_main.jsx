3d64988cdba270af761d126148261a6fbf0e64f8
HyeongJun, Kim <khj029956@naver.com>
2024-06-22 17:51:38 +0900

    Feat: Home 대폭 수정

--- front/src/Home/home_main.jsx
+++ front/src/Home/home_main.jsx
@@ -1,213 +1,90 @@
 import React, { useState, useEffect } from 'react';
-import './home_main.css';
-import { SlArrowRight } from 'react-icons/sl';
-import { AiOutlineQuestionCircle } from 'react-icons/ai'; // 툴팁 아이콘 import
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { formatDistanceToNow, parseISO } from 'date-fns';
-import { ko } from 'date-fns/locale'; // 한국어 로케일 import
+import './home_main.css';
 
-const Main = () => {
+const HomeMain = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slides = [
+    {
+      src: process.env.PUBLIC_URL + '/images/home_image/home-card-1.png',
+      link: '/contribute_ranking',
+      message: '기여도 랭킹 확인하기',
+    },
+    {
+      src: process.env.PUBLIC_URL + '/images/home_image/home-card-2.png',
+      link: '/post_main',
+      message: '전체 게시글 보러가기',
+    },
+  ];
   const navigate = useNavigate();
 
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  // 사용자 정보를 저장할 상태
-  const [userInfos, setUserInfos] = useState({});
-
-  const [recommendedPosts, setRecommendedPosts] = useState([]);
-  const [newestPosts, setNewestPosts] = useState([]);
-  const [ranking, setRanking] = useState([]);
-
-  const fetchUserInfos = async (userIds) => {
-    const userInfoPromises = userIds.map((id) =>
-      axios
-        .get(`http://localhost:3000/mypage/${id}`)
-        .then((response) => ({
-          userId: id,
-          data: response.data,
-        }))
-        .catch(() => ({
-          userId: id,
-          data: { nickname: '탈퇴한 회원' }, // 사용자 정보가 없는 경우 처리
-        }))
-    );
-
-    const userInfoResponses = await Promise.all(userInfoPromises);
-
-    const newUserInfos = {};
-    userInfoResponses.forEach((response) => {
-      newUserInfos[response.userId] = response.data;
-    });
-
-    setUserInfos((prevUserInfos) => ({
-      ...prevUserInfos,
-      ...newUserInfos,
-    }));
-  };
-
-  // 날짜 형식 변환 함수
-  const formatDate = (dateString) => {
-    const date = parseISO(dateString);
-    const now = new Date();
-    const differenceInDays = (now - date) / (1000 * 60 * 60 * 24);
-
-    if (differenceInDays < 1) {
-      return formatDistanceToNow(date, { addSuffix: true, locale: ko });
-    } else {
-      return date.toLocaleDateString('ko-KR');
-    }
-  };
-
-  // 게시글 클릭 핸들러
-  const handlePostClick = (post) => {
-    navigate(`/post_view/${post.post_id}`, { state: { post } });
-  };
-
   useEffect(() => {
-    const fetchPostsAndRankings = async () => {
-      try {
-        const [latestResponse, likesResponse, rankingResponse] =
-          await Promise.all([
-            axios.get('http://localhost:3000/post/latest'),
-            axios.get('http://localhost:3000/post/mostlike'),
-            axios.get('http://localhost:3000/post/top-contributors'),
-          ]);
-
-        const newestPostsData = latestResponse.data.slice(0, 5);
-        const recommendedPostsData = likesResponse.data.slice(0, 5);
-        const rankingData = rankingResponse.data;
-
-        const userIds = [
-          ...new Set([
-            ...newestPostsData.map((post) => post.user_id),
-            ...recommendedPostsData.map((post) => post.user_id),
-            ...rankingData.map((rank) => rank.user_id),
-          ]),
-        ];
-
-        await fetchUserInfos(userIds);
-
-        setNewestPosts(newestPostsData);
-        setRecommendedPosts(recommendedPostsData);
-        setRanking(rankingData);
-        setLoading(false);
-      } catch (err) {
-        setError(`데이터를 가져오는 데 실패했습니다: ${err.message}`);
-        setLoading(false);
-      }
-    };
-
-    fetchPostsAndRankings();
+    const interval = setInterval(nextSlide, 5000); // 5초 간격으로 슬라이드
+    return () => clearInterval(interval);
   }, []);
 
-  if (loading) {
-    return <div>로딩 중...</div>;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
-
-  const languageIcons = {
-    c: '/images/language_icons/c_icon.png',
-    cpp: '/images/language_icons/cpp_icon.png',
-    java: '/images/language_icons/java_icon.png',
-    python: '/images/language_icons/python_icon.png',
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
   };
 
-  const contribute_post_like = 1;
-  const contribute_feedback = 1;
-  const contribute_feedback_like = 1;
+  const prevSlide = () => {
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + slides.length) % slides.length
+    );
+  };
+
+  const goToLink = (link) => {
+    navigate(link);
+  };
 
   return (
     <div className="home-container">
-      <section className="home_left">
-        <div className="ranking">
-          <h2>
-            기여도 랭킹
-            <AiOutlineQuestionCircle className="tooltip-icon" />
-            <div className="tooltip">
-              기여도는 (게시물 좋아요 x {contribute_post_like}) + (내가 달은
-              피드백 x {contribute_feedback}) + (내 피드백 좋아요 x{' '}
-              {contribute_feedback_like}) 의 연산입니다.
+      <div className="slider">
+        <div
+          className="slides"
+          style={{
+            transform: `translateX(-${currentSlide * 100}%)`,
+          }}
+        >
+          {slides.map((slide, index) => (
+            <div className="slide" key={index}>
+              <img src={slide.src} alt={`slide-${index + 1}`} />
+              <button
+                className="slide-button"
+                onClick={() => goToLink(slide.link)}
+              >
+                {slide.message}
+              </button>
             </div>
-          </h2>
-          <ul className="ranking-list">
-            {ranking.map((user, index) => (
-              <li key={index}>
-                <div className="ranking-meta">
-                  <div className="ranking-user-id">
-                    {userInfos[user.user_id]?.nickname || '탈퇴한 회원'}
-                  </div>
-                  <div className="ranking-contribution">
-                    {user.total_contribution}
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+          ))}
         </div>
-      </section>
-      <section className="home_mid">
-        <div className="post recommend">
-          <h2>추천 게시글 (좋아요 순)</h2>
-          <ul className="postcode">
-            {recommendedPosts.map((post, index) => (
-              <li key={index} onClick={() => handlePostClick(post)}>
-                <div className="post-main-meta">
-                  <div className="post-main-title">
-                    <img
-                      src={languageIcons[post.language]}
-                      alt=""
-                      className="post-main-language-icon"
-                    />{' '}
-                    {post.post_title}
-                  </div>
-                  <div className="post-main-user-name">
-                    {userInfos[post.user_id]?.nickname || '탈퇴한 회원'}
-                  </div>
-                  <div className="post-main-date">
-                    {formatDate(post.post_date)}
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="post newest">
-          <h2>최신 게시글 (시간 순)</h2>
-          <ul className="postcode">
-            {newestPosts.map((post, index) => (
-              <li key={index} onClick={() => handlePostClick(post)}>
-                <div className="post-main-meta">
-                  <div className="post-main-title">
-                    <img
-                      src={languageIcons[post.language]}
-                      alt=""
-                      className="post-main-language-icon"
-                    />{' '}
-                    {post.post_title}
-                  </div>
-                  <div className="post-main-user-name">
-                    {userInfos[post.user_id]?.nickname || '탈퇴한 회원'}
-                  </div>
-                  <div className="post-main-date">
-                    {formatDate(post.post_date)}
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section>
-      <section className="home_right" onClick={() => navigate('/post_main')}>
-        <SlArrowRight style={{ fontSize: '60px' }} />
-        <div>전체 보기</div>
-      </section>
+        <button className="prev-button" onClick={prevSlide}>
+          ‹
+        </button>
+        <button className="next-button" onClick={nextSlide}>
+          ›
+        </button>
+      </div>
+      <div className="images-container">
+        <img
+          src={process.env.PUBLIC_URL + '/images/home_image/home-img-1.png'}
+          alt="home-img-1"
+          className="home-img"
+        />
+        <img
+          src={process.env.PUBLIC_URL + '/images/home_image/home-img-2.png'}
+          alt="home-img-2"
+          className="home-img"
+        />
+      </div>
+      <div className="end-card">
+        <img
+          src={process.env.PUBLIC_URL + '/images/home_image/home-end-card.png'}
+          alt="home-end-card"
+        />
+      </div>
     </div>
   );
 };
 
-export default Main;
+export default HomeMain;
