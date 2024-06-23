1297c146b88f696626fc786521f226f010500375
hnk1203 <hnk120325@gmail.com>
2024-06-23 17:21:28 +0900

    error test

--- front/src/Admin/Admin_notice_update.jsx
+++ front/src/Admin/Admin_notice_update.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './admin_notice_create.css';
-import Editor from '@toast-ui/editor';
-import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
-import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from "react";
+import "./admin_notice_create.css";
+import Editor from "@toast-ui/editor";
+import "@toast-ui/editor/dist/toastui-editor.css"; // Editor's Style
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 const AdminNoticeUpdate = () => {
   //에디터 생성 및 해당 게시글 정보 가져오기 관련
   const editorRef = useRef(null);
   const editorInstanceRef = useRef(null);
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const { id } = useParams();
 
   const navigate = useNavigate();
@@ -37,12 +37,42 @@ const AdminNoticeUpdate = () => {
     if (editorRef.current) {
       editorInstanceRef.current = new Editor({
         el: editorRef.current,
-        height: '600px',
+        height: "600px",
         initialValue: content,
-        initialEditType: 'wysiwyg',
-        previewStyle: 'vertical',
-        language: 'ko-KR',
+        initialEditType: "wysiwyg",
+        previewStyle: "vertical",
+        language: "ko-KR",
         hideModeSwitch: true,
+        hooks: {
+          async addImageBlobHook(blob, callback) {
+            try {
+              const formData = new FormData();
+              formData.append("image", blob);
+
+              // axios로 서버에 FormData 전송
+              await axios
+                .post("http://localhost:3000/notice/image", formData, {
+                  headers: {
+                    "Content-Type": "multipart/form-data",
+                  },
+                })
+                .then((response) => {
+                  if (response.data.hi === "hello") {
+                    const filename = `/images/post_notice/${blob.name}`;
+                    console.log("서버에 저장된 파일명 : ", filename);
+                    console.log(formData.get("image"));
+                    // 이미지 URL을 에디터에 렌더링
+                    const imageUrl = `${filename}`;
+                    callback(imageUrl, "image alt attribute");
+                  } else {
+                    console.error("서버에서 파일명을 받지 못했습니다.");
+                  }
+                });
+            } catch (error) {
+              console.error("업로드 실패 : ", error);
+            }
+          },
+        },
       });
     }
   }, [content]);
@@ -58,10 +88,10 @@ const AdminNoticeUpdate = () => {
         .then((Response) => {
           if (Response.data.success === true) {
             console.log(editorInstanceRef.current.getMarkdown());
-            alert('수정 성공!!');
-            navigate('/admin');
+            alert("수정 성공!!");
+            navigate("/admin");
           } else if (Response.data.success === false) {
-            alert('수정 실패! 다시 시도해주세요');
+            alert("수정 실패! 다시 시도해주세요");
           }
           console.log(Response);
         })
